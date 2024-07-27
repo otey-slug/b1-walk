2716fe968c244d0dda4abc16db122c493b86ea6a
inkyant <80181074+inkyant@users.noreply.github.com>
2024-07-27 16:46:14 -0700

    rename to b1-walk

--- exts/b1-walk/setup.py
+++ exts/b1-walk/setup.py
@@ -1,4 +1,4 @@
-"""Installation script for the 'ext_template' python package."""
+"""Installation script for the 'b1-walk' python package."""
 
 import os
 import toml
@@ -18,8 +18,8 @@ INSTALL_REQUIRES = [
 
 # Installation operation
 setup(
-    name="ext_template",
-    packages=["ext_template"],
+    name="b1-walk",
+    packages=["b1-walk"],
     author=EXTENSION_TOML_DATA["package"]["author"],
     maintainer=EXTENSION_TOML_DATA["package"]["maintainer"],
     url=EXTENSION_TOML_DATA["package"]["repository"],
